206db5763f9df7c95c744e168001cb34f93245b5
Эдуард <eduard.bzian@gmail.com>
2024-08-06 20:59:51 +0300

    module_9_1.py

---
+++ module_9_1.py
@@ -0,0 +1,7 @@
+def apply_all_func(int_list, *functions):
+    reuslts = {}
+    for func in functions:
+        reuslts[func.__name__] = func(int_list)
+    return reuslts
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
